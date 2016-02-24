59c8433c4768b4aff9b24fc22097d7db6a8a01ea
Paul Andrieux <paul@appventus.com>
2016-02-24 15:19:31 +0100

    :arrow_up: Symfony ~2.8 | ~3.0

--- Form/WidgetCoverType.php
+++ Form/WidgetCoverType.php
@@ -3,53 +3,37 @@
 namespace Victoire\Widget\CoverBundle\Form;
 
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Victoire\Widget\ImageBundle\Form\WidgetImageType;
 
-/**
- * WidgetCover form type.
- */
 class WidgetCoverType extends WidgetImageType
 {
     /**
      * define form fields.
      *
      * @param FormBuilderInterface $builder
-     *
      * @param array $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         parent::buildForm($builder, $options);
         $builder->add('opacity', null, [
-                    'label'          => 'widget_cover.form.opacity.label',
-                    'vic_help_block' => 'widget_cover.form.opacity.help_block',
-                ]);
+            'label'          => 'widget_cover.form.opacity.label',
+            'vic_help_block' => 'widget_cover.form.opacity.help_block',
+        ]);
     }
 
     /**
-     * bind form to WidgetCover entity.
-     *
-     * @param OptionsResolverInterface $resolver
-     */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    * {@inheritdoc}
+    */
+    public function configureOptions(OptionsResolver $resolver)
     {
-        parent::setDefaultOptions($resolver);
+        parent::configureOptions($resolver);
 
         $resolver->setDefaults([
             'data_class'         => 'Victoire\Widget\CoverBundle\Entity\WidgetCover',
             'widget'             => 'Cover',
             'translation_domain' => 'victoire',
         ]);
-    }
-
-    /**
-     * get form name.
-     *
-     * @return string The form name
-     */
-    public function getName()
-    {
-        return 'victoire_widget_form_cover';
     }
 }
